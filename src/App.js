cf9be956bfc7f7a169b4cdc22858cc48221f98b5
Brendan McCaughey <brendan.mccaughey@workgrid.com>
2020-12-08 20:51:12 -0500

    Added prettier - some layout

--- src/App.js
+++ src/App.js
@@ -1,49 +1,163 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useForm } from "react-hook-form";
-import { ErrorMessage } from '@hookform/error-message';
-import styled from 'styled-components'
+import { ErrorMessage } from "@hookform/error-message";
+import styled from "styled-components";
+
+import gratitude from "./assets/gratitude.jpg";
+import gamechanger from "./assets/gamechanger.jpg";
+import cheersforpeers from "./assets/cheersforpeers.jpg";
+import raisetheroof from "./assets/raisetheroof.jpg";
+
+const StyledWrapper = styled.div`
+  margin: 20px;
+`;
+
+const StyledHeader = styled.h1`
+  text-align: center;
+`;
+
+const StyledForm = styled.form`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 10px;
+  grid-auto-rows: minmax(100px, auto);
+`;
+
+const StyledInputContainer = styled.div`
+  grid-column: 1;
+  grid-row: 1 / 5;
+`;
+
+const StyledRadioContainer = styled.div`
+  grid-column: 2 / 4;
+  grid-row: 1 / 3;
+`;
+
+const StyledImage = styled.img`
+  width: 70px;
+  height: auto;
+`;
+
+const StyledValidation = styled.p`
+  color: #bf1650;
+  ::before {
+    display: inline;
+    content: "⚠ ";
+  }
+`;
 
 export default function App() {
-  const { register, handleSubmit, watch, errors } = useForm();
-  const onSubmit = data => console.log(data);
+  const { register, handleSubmit, errors } = useForm();
+  const onSubmit = (data) => console.log(data);
 
-  console.log(watch("example")); // watch input value by passing the name of it
+  useEffect(() => {
+    async function fetchUsers() {
+      const TOKEN =
+        "YjQ4a2syZzc5bGFjNDUxa3RjdGRrc3ZvNzoxOTc0NnNsN3A2cmtyYWp1MWpxcGczZm1haXFpNGkxaHNoaDkzZmFrdWp0aW5ybHZhYWVy";
+      const response = await fetch(
+        "https://yfvnkbux6j.execute-api.us-east-1.amazonaws.com/users/",
+        {
+          method: "GET",
+          mode: "no-cors",
+          headers: { Authorization: `Bearer ${TOKEN}` },
+        }
+      );
+      console.log(response);
+    }
+    fetchUsers();
+  }, []);
 
   return (
-    <>
-    <h1>Workgrid Rockstar!</h1>
-  <form onSubmit={handleSubmit(onSubmit)}>
-    <p>Who would you like to recognize?</p>
-    <input type="text" placeholder="Recipient Email" name="RecipientEmail" ref={register({required: true, pattern: /^\S+@\S+$/i})} />
-    {errors.recipientEmailRequired && <span>This field is required</span>}
-    <ErrorMessage
-      errors={errors}
-      name="RecipientEmail"
-      render={({ messages }) => {
-        console.log("messages", messages);
-        return messages
-          ? Object.entries(messages).map(([type, message]) => (
-            <p key={type}>{message}</p>
-          ))
-          : null;
-      }}
-    />
-    <p>Tell us why you think they are a rockstar...</p>
-    <textarea name="Summary" placeholder="Add a message..." ref={register({ required: true })}/>
-    {errors.summaryRequired && <span>This field is required</span>}
-    <p>Select your gift level...</p>
-    <input type="radio" id="Starbucks" name="Gift" value="Starbucks5" ref={register({ required: true })}/>
-    <label for="Starbucks5">Gratitude: $5 Starbucks Gift Card</label>
-    <input type="radio" id="Starbucks" name="Gift" value="Starbucks10" ref={register({ required: true })}/>
-    <label htmlFor="Starbucks10">Cheers for Peers: $10 Starbucks Gift Card</label>
-    <input type="radio" id="Amazon" name="Gift" value="Amazon20" ref={register({ required: true })}/>
-    <label for="Amazon20">Game Changer: $20 Amazon Gift Card</label>
-    <input type="radio" id="Amazon" name="Gift" value="Amazon25" ref={register({ required: true })}/>
-    <label htmlFor="Amazon25">Raise the Roof: $25 Amazon Gift Card</label>
-
-    <p/>
-    <input type="submit" />
-  </form>
-    </>
-);
+    <StyledWrapper>
+      <StyledHeader>Workgrid Rockstar!</StyledHeader>
+      <StyledForm onSubmit={handleSubmit(onSubmit)}>
+        <StyledInputContainer>
+          <p>Who would you like to recognize?</p>
+          <input
+            type="text"
+            placeholder="Recipient Email"
+            name="RecipientEmail"
+            ref={register({
+              required: "This input is required.",
+              pattern: /^\S+@\S+$/i,
+            })}
+          />
+          <ErrorMessage
+            errors={errors}
+            name="RecipientEmail"
+            render={({ messages }) => {
+              console.log("messages", messages);
+              return messages
+                ? Object.entries(messages).map(([type, message]) => (
+                    <StyledValidation key={type}>{message}</StyledValidation>
+                  ))
+                : null;
+            }}
+          />
+          <p>Tell us why you think they are a rockstar...</p>
+          <textarea
+            name="text"
+            placeholder="Add a message..."
+            ref={register({ required: "This input is required." })}
+          />
+          <ErrorMessage
+            errors={errors}
+            name="text"
+            render={({ messages }) => {
+              console.log("messages", messages);
+              return messages
+                ? Object.entries(messages).map(([type, message]) => (
+                    <StyledValidation key={type}>{message}</StyledValidation>
+                  ))
+                : null;
+            }}
+          />
+        </StyledInputContainer>
+        <StyledRadioContainer>
+          <p>Select your gift level...</p>
+          <input
+            type="radio"
+            id="Starbucks"
+            name="level"
+            value="5"
+            ref={register({ required: true })}
+          />
+          <StyledImage src={gratitude} />
+          <label for="5">Gratitude: $5 Starbucks Gift Card</label>
+          <p />
+          <input
+            type="radio"
+            id="Starbucks"
+            name="level"
+            value="10"
+            ref={register({ required: true })}
+          />
+          <StyledImage src={cheersforpeers} />
+          <label for="10">Cheers for Peers: $10 Starbucks Gift Card</label>
+          <p />
+          <input
+            type="radio"
+            id="Amazon"
+            name="level"
+            value="20"
+            ref={register({ required: true })}
+          />
+          <StyledImage src={gamechanger} />
+          <label for="20">Game Changer: $20 Amazon Gift Card</label>
+          <p />
+          <input
+            type="radio"
+            id="Amazon"
+            name="level"
+            value="25"
+            ref={register({ required: true })}
+          />
+          <StyledImage src={raisetheroof} />
+          <label for="25">Raise the Roof: $25 Amazon Gift Card</label>
+          <p />
+          <input type="submit" />
+        </StyledRadioContainer>
+      </StyledForm>
+    </StyledWrapper>
+  );
 }
