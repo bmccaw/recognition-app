0985c28627fdb5faffa9fe5939f1a9031040a9e6
Finbar Clenaghan <finbar.clenaghan@liberty-it.co.uk>
2020-12-09 20:19:25 +0000

    Add bootstrap

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const routes = (
   <BrowserRouter>
